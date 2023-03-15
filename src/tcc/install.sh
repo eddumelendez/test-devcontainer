bea424d41b6229606593b46b47d0a9d2ea8d8364
Eddú Meléndez <eddu.melendez@gmail.com>
2023-03-14 19:10:38 -0600

    Add entrypoint to start tcc.sh

--- src/tcc/install.sh
+++ src/tcc/install.sh
@@ -8,7 +8,5 @@ echo "The provided version is: $VERSION"
 
 LATEST_BINARY_URL="https://app.testcontainers.cloud/download/testcontainers-cloud-agent_${VERSION}_linux_x86-64"
 
-curl -fsSL -o tcc "${LATEST_BINARY_URL}"
-chmod +x tcc
-
-./tcc &
+curl -fsSL -o /usr/local/share/tcc "${LATEST_BINARY_URL}"
+chmod +x /usr/local/share/tcc

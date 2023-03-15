ef706de801a96809d2fffbec10972bf4136f2566
Eddú Meléndez <eddu.melendez@gmail.com>
2023-03-14 18:19:04 -0600

    Initial commit

---
+++ src/tcc/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'testcontainers-cloud'"
+
+VERSION=${VERSION:-undefined}
+echo "The provided version is: $VERSION"
+
+LATEST_BINARY_URL="https://app.testcontainers.cloud/download/testcontainers-cloud-agent_${VERSION}_linux_x86-64"
+
+curl -fsSL -o tcc "${LATEST_BINARY_URL}"
+chmod +x tcc
+
+./tcc &

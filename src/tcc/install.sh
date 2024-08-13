b2d971d4ac8e7db7ae62dfe758a1969c0994f1d3
Ganna Chernyshova <anna@atomicjar.com>
2024-08-13 12:00:59 -0400

    Updated install script to determine the os for the agent and architecture of the platform, updated postStartCommand to start the agent and generate log (#1)
    Co-authored-by: Eddú Meléndez Gonzales <eddu.melendez@gmail.com>

--- src/tcc/install.sh
+++ src/tcc/install.sh
@@ -3,11 +3,29 @@ set -e
 
 echo "Activating feature 'testcontainers-cloud'"
 
+# determine tcc version
 VERSION=${VERSION:-undefined}
 echo "The provided version is: $VERSION"
 
-LATEST_BINARY_URL="https://app.testcontainers.cloud/download/testcontainers-cloud-agent_${VERSION}_linux_x86-64"
+# determine the os for the agent
+platform="$(uname -s)"
+case "${platform}" in
+    Linux*)     OS_TYPE=linux;;
+    Darwin*)    OS_TYPE=darwin;;
+    *)          say "${r}This script doesn't know how to deal with${x} ${platform} ${r}os type!${x}"; exit 1
+esac
 
-curl -fsSL -o /usr/local/share/tcc "${LATEST_BINARY_URL}"
-chmod +x /usr/local/share/tcc
-chown root:root /usr/local/share/tcc
+# determine the architecture of the platform
+arch="$(uname -m)"
+case "${arch}" in
+    aarch64*)  ARCH_TYPE=arm64;;
+    arm64*)    ARCH_TYPE=arm64;;
+    *)         ARCH_TYPE=x86-64;;
+esac
+
+LATEST_BINARY_URL="https://app.testcontainers.cloud/download/testcontainers-cloud-agent_${VERSION}_${OS_TYPE}_${ARCH_TYPE}"
+
+echo "Downloading ${LATEST_BINARY_URL}"
+curl -fsSL -o /usr/local/share/tcc-agent ${LATEST_BINARY_URL}
+chmod +x /usr/local/share/tcc-agent
+chown root:root /usr/local/share/tcc-agent
